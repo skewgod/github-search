987f40928956f47c0eceae514da983bf4d926e5e
Ganesh Rai <ganesh.rai@ril.com>
2024-08-26 22:12:19 +0530

    changed default search type

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@ export default {
     return {
       items: [],
       loading: false,
-      searchType: 'repositories' // Default search type
+      searchType: 'users'
     }
   },
   computed: {
